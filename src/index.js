9565bbe8cdd8f09ff09697b7363c82827a2b09e8
Bonganibg <bongani.gumbo@yahoo.com>
2024-04-21 17:35:13 +0200

    refactored file search function

--- src/index.js
+++ src/index.js
@@ -39,16 +39,8 @@ console.log(auth);
 
 let dbx  = auth.createDbxConnection();
 
-// ----------------------------------------------------------------------------------------
-// ----------------------------SET UP BUTTON STUFF ----------------------------------------
-// ----------------------------------------------------------------------------------------
-
 //Don't check token on review page
 reviewTimerService.setUpTimer(createUI)
-
-// ----------------------------------------------------------------------------------------
-// ----------------------------SET UP BUTTON STUFF ----------------------------------------
-// ----------------------------------------------------------------------------------------
 
 //! Step 2:  Create Floating UI popup
 function createUI() {
@@ -108,163 +100,167 @@ function createUI() {
   document.body.appendChild(floatingElement);
 
   // Call function to get page values and update UI elements
-  extractionService.extractStudentNumber(filesSearch); //! Step 2.1 : Call function to "Extract the student number"
+  extractionService.extractStudentNumber(loadDropboxFiles); //! Step 2.1 : Call function to "Extract the student number"
 
   //reviewTimer()
   getReviewCounts();
 }
-``
 
-//! Step 3 Find a specific file by name - Display results
-//if more then one root folder exists for example "myFolder and myFolder (1)...",
-//first search for my myFolder, then using an incrementor to control the value
-//between the brackets, and making up to 3 searches each time adding its results to the UI
 
-async function filesSearch(studentNumber, taskName) {
-  let query = taskName;
 
-  //update "build your brand" strings
-  //! BYB tasks files are sometimes written as byb 04 or byb IV. But on the review page it is always written with Roman numerals
-  //THis function will only update task name if the dropbox files contains number instead of roman numerals.
+
+
+
+
+
+
+
+
+
+
+
+function filterBybName(query){  
   let pattern = /build your brand/i;
+
   if (query.match(pattern) && /(01|02|03|04|05)/.test(query)) {
-    query = replaceRomanNumeralsWithNumbers(query).toLowerCase();
+    return replaceRomanNumeralsWithNumbers(query).toLowerCase();
+  }  
+
+  return query
+}
+
+function hideSpinner(content){
+  if (!shared.removeSpinner)
+    return
+
+  shared.routeList.innerHTML = content;
+  shared.removeSpinner = false;
+}
+
+function createTaskSearchResultComponents(item){
+  let btnAndListContainer = document.createElement("div");
+  let foundRes = document.createElement("div");
+  let dlIconContainer = document.createElement("div");
+  let dlIcon = document.createElement("img");
+  let linkIcon = document.createElement("img");
+
   
-  }
+  btnAndListContainer.className = "DBXFF-btnAndListContainer";
+  foundRes.className = "DBXFF-foundRes";
+  foundRes.textContent = item.metadata.metadata.path_display;
+  dlIconContainer.className = "DBXFF-dlIconContainer";
+  linkIcon.title = "Open Task Folder in dropbox";
+  linkIcon.src = chrome.runtime.getURL("images/externalLink.png");
 
-  console.log('query', query)
+  let type = document.createElement("img");
+  type.alt = item.metadata.metadata.name;
+  dlIcon.src = chrome.runtime.getURL("images/dlFOlder.png");
+  dlIcon.alt = "dl-icon";
+  dlIcon.className = "DBXFF-dl-icon-list";
+  dlIcon.title = "Download Task folder";
 
-  console.log(`%c Searching for files ${shared.inc}`, "color: #5390d9");
+  dlIconContainer.appendChild(dlIcon);
+  btnAndListContainer.appendChild(dlIconContainer);
+  btnAndListContainer.appendChild(linkIcon);
+  btnAndListContainer.appendChild(foundRes);
+  shared.routeList.appendChild(btnAndListContainer);
+
+  linkIcon.addEventListener("click", async (e) => {
+    let str = item.metadata.metadata.path_display;
+    let lastSlashIndex = str.lastIndexOf("/");
+    let newStr = str.substring(0, lastSlashIndex);
+    let link = "https://www.dropbox.com/work/HyperionDev%20Reviewers" +
+                newStr.replace(" ", "%20");
+
+    window.open(link, "_blank");
+  });
+
+
+  dlIcon.addEventListener("click", async (e) => {
+    e.stopPropagation(); //prevent the route from being selected
+    dlIcon.classList.add("rotate-center");
+    dlIcon.src = chrome.runtime.getURL("images/loader.png");
+    //! Download selected Folder or file
+    dropboxService.downloadFolder(item.metadata.metadata, dlIcon, dbx); //folder
+  });
+}
+
+async function getFileSearch(studentNumber, query){  
   let root = studentNumber;
   let retry = shared.inc;
 
   const path = shared.inc > 0 ? root + ` (${retry})` : root;
-
-  // Call the Dropbox API to search for a file
-  await dbx
-    .filesSearchV2({
+  
+  
+  try{    
+    let response = await dbx.filesSearchV2({
       query: query,
       options: {
         path: "/" + path,
-        //file_extensions: [".pdf"],
-        max_results: 5,
-      },
-    })
-    .then(function (response) {
-      if (shared.removeSpinner) {
-        shared.routeList.innerHTML = "";
-        shared.removeSpinner = false;
+        max_results: 5
       }
-
-      console.log(`%c  making request: ${shared.inc}`, "color: orange");
-      shared.inc++; //after 1rst request look for a 2nd folder
-      let results = response.result.matches;
-
-      //if results are 0 and we did 3 searches already? stop
-      if (shared.inc >= 4) {
-        highlightTaskName(query);
-
-        return;
-      } else {
-        if (!shared.foundFiles) {
-          //foundFiles = true;
-          console.log(`%c results found: ${results.length}`, "color: #2196f3");
-       
-          //Creates a list of all the results results
-          //Aldo build the dive that contains the download button and the link to the folder
-          results.forEach((item, i) => {
-            let taskNumber = item.metadata.metadata.path_display
-            console.log('taskNum', shared.taskNum)
-            // Only display the results that contain the task number
-            if (taskNumber.includes(shared.taskNum)) {
-              if (shared.taskNum < 10) {
-                shared.taskNum = "T0" + shared.taskNum[shared.taskNum.length - 1];
-               
-               }
-
-              let btnAndListContainer = document.createElement("div");
-              btnAndListContainer.className = "DBXFF-btnAndListContainer";
-              let foundRes = document.createElement("div");
-              foundRes.className = "DBXFF-foundRes";
-              foundRes.textContent = item.metadata.metadata.path_display;
-              let dlIconContainer = document.createElement("div");
-              dlIconContainer.className = "DBXFF-dlIconContainer";
-              let dlIcon = document.createElement("img");
-              let linkIcon = document.createElement("img");
-              linkIcon.title = "Open Task Folder in dropbox";
-              linkIcon.src = chrome.runtime.getURL("images/externalLink.png");
-              linkIcon.addEventListener("click", async (e) => {
-                let str = item.metadata.metadata.path_display;
-
-                // Find the last index of "/"
-                let lastSlashIndex = str.lastIndexOf("/");
-
-                // Remove everything after the last "/"
-                let newStr = str.substring(0, lastSlashIndex);
-
-                // Replace all spaces with "%20"
-                let link =
-                  "https://www.dropbox.com/work/HyperionDev%20Reviewers" +
-                  newStr.replace(" ", "%20");
-
-                // Open the folder URL in a new tab
-                window.open(link, "_blank");
-              });
-              let type = document.createElement("img");
-              type.alt = item.metadata.metadata.name;
-              dlIcon.src = chrome.runtime.getURL("images/dlFOlder.png");
-              dlIcon.alt = "dl-icon";
-              dlIcon.className = "DBXFF-dl-icon-list";
-              dlIcon.title = "Download Task folder";
-              dlIcon.addEventListener("click", async (e) => {
-                e.stopPropagation(); //prevent the route from being selected
-                dlIcon.classList.add("rotate-center");
-                dlIcon.src = chrome.runtime.getURL("images/loader.png");
-                //! Download selected Folder or file
-                dropboxService.downloadFolder(item.metadata.metadata, dlIcon, dbx); //folder
-              });
-
-              dlIconContainer.appendChild(dlIcon);
-              btnAndListContainer.appendChild(dlIconContainer);
-              btnAndListContainer.appendChild(linkIcon);
-              btnAndListContainer.appendChild(foundRes);
-              shared.routeList.appendChild(btnAndListContainer);
-
-            }
-
-          });
-
-          //look in 2nd and 3rd folder
-          if (shared.inc >= 4) {
-            return;
-          }
-          filesSearch(studentNumber, taskName);
-        }
-      }
-    })
-    .catch(async function (error) {
-      console.log(error);
-      if (shared.removeSpinner) {
-
-        shared.routeList.innerHTML = `
-        <p>Either the token has expired. A popup will appear. (If not - Refresh Browser)</p>
-        <br>
-        <p>Or the Task Name could no be found inside student folders</p>
-        <br>
-        <p>Or CoGrammar API is offline</p>
-        <br>
-        <p>Or you have an internet connection issue</p>
-        <br>
-        <p>Try looking up the student number in Dropbox</p>
-        `;
-        shared.removeSpinner = false;
-      }
-      console.log(`%c Search ended`, "color: hotpink");
-      await auth.checkToken(dbx);
-
-      return;
     });
+
+    let result = await response.result.matches;
+    return result;
+  } catch(err){
+    console.log(err);  
+  }
+  
+  return undefined;
 }
+
+async function loadDropboxFiles(studentNumber, taskName){ 
+  let query = filterBybName(taskName); //update "build your brand" strings  
+  let response = await getFileSearch(studentNumber, query);
+
+  if (response == undefined){
+    let content = `
+      <p>Either the token has expired. A popup will appear. (If not - Refresh Browser)</p>
+      <br>
+      <p>Or the Task Name could no be found inside student folders</p>
+      <br>
+      <p>Or CoGrammar API is offline</p>
+      <br>
+      <p>Or you have an internet connection issue</p>
+      <br>
+      <p>Try looking up the student number in Dropbox</p>
+      `;
+
+      hideSpinner(content);
+
+      await auth.checkToken(dbx)
+      return;
+  }
+
+  hideSpinner("");
+  shared.inc++;
+
+  if (shared.inc >= 4){
+    highlightTaskName(query)
+    return;
+  }
+
+  if (shared.foundFiles)
+    return;
+
+  response.forEach((item, i) => {
+    let taskNumber = item.metadata.metadata.path_display;
+
+    if (taskNumber.includes(shared.taskNum)){
+      if (shared.taskNum < 10)
+        shared.taskNum = "T0" + shared.taskNum[shared.taskNum.length - 1];
+
+      createTaskSearchResultComponents(item);
+    }
+  });
+
+  if (shared.inc >= 4)
+    return;
+
+  loadDropboxFiles(studentNumber, taskName);    
+}
+
 //Extracts the words that matches the task name and only highlight those words.
 function highlightTaskName(taskName) {
 
@@ -369,25 +365,5 @@ function replaceRomanNumeralsWithNumbers(inputString) {
   // Use the replace() method with a callback function to replace Roman numerals with numbers
   return inputString.replace(romanNumeralRegex, (match, romanNumeral) => {
     return romanToNumberMap[romanNumeral] || match;
-  });
-}
-
-
-function highlightTaskNumber() {
-  // Loop through each list item
-  listItems.forEach((item) => {
-
-    // Create a regular expression to match "Task" followed by a space, one or more digits, and a hyphen "-"
-    let regex = /Task \d+/gi;
-
-    // Test if the search word is found in the list item's text content
-    if (regex.test(item.textContent)) {
-
-      // If the search word is found, replace it with a highlighted version
-      let found = item.innerHTML.replace(regex, `<b class="highlight">$&</b>`);
-
-      // Replace the original list item HTML with the highlighted version
-      item.innerHTML = found;
-    }
   });
 }
